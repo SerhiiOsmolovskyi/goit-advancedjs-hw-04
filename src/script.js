8967387b229cf4444a9aa93729950335a10ae3f8
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2023-11-28 16:06:53 -0700

    LightBox Fixed

--- src/script.js
+++ src/script.js
@@ -14,136 +14,147 @@ let page = 1;
 const lightbox = new SimpleLightbox('.gallery a');
 
 searchForm.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const searchQuery = e.target.searchQuery.value.trim();
+  e.preventDefault();
+  const searchQuery = e.target.searchQuery.value.trim();
 
-    if (searchQuery === '') {
-        return;
-    }
+  if (searchQuery === '') {
+    return;
+  }
 
-    page = 1;
+  page = 1;
 
-    try {
-        const images = await fetchImages(searchQuery, page);
-        renderImages(images);
-        lightbox.refresh();
-    } catch (error) {
-        console.error('Error fetching images:', error);
-    }
+  try {
+    const images = await fetchImages(searchQuery, page);
+    renderImages(images);
+    console.log('render');
+    lightbox.refresh();
+  } catch (error) {
+    console.error('Error fetching images:', error);
+  }
 });
 
 loadMoreBtn.addEventListener('click', async function () {
-    const searchQuery = searchForm.searchQuery.value.trim();
+  const searchQuery = searchForm.searchQuery.value.trim();
 
-    if (searchQuery === '') {
-        return;
-    }
+  if (searchQuery === '') {
+    return;
+  }
 
-    try {
-        page++;
-        const images = await fetchImages(searchQuery, page);
-        renderImages(images);
-        lightbox.refresh();
-    } catch (error) {
-        console.error('Error fetching more images:', error);
-    }
+  try {
+    page++;
+    const images = await fetchImages(searchQuery, page);
+    renderImages(images);
+    lightbox.refresh();
+  } catch (error) {
+    console.error('Error fetching more images:', error);
+  }
 });
 
 async function fetchImages(query, page) {
-    try {
-        const response = await axios.get(API_URL, {
-            params: {
-                key: apiKey,
-                q: query,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-                page: page,
-                per_page: 40,
-            },
-        });
+  try {
+    const response = await axios.get(API_URL, {
+      params: {
+        key: apiKey,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: 40,
+      },
+    });
 
-        const { hits, totalHits } = response.data;
+    const { hits, totalHits } = response.data;
 
-        if (totalHits === 0) {
-            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-            hideLoadMoreButton();
-            return [];
-        }
-
-        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-        showLoadMoreButton();
-
-        return hits;
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        Notiflix.Notify.failure('An error occurred while fetching images. Please try again.');
-        return [];
+    if (totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      hideLoadMoreButton();
+      return [];
     }
+
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    showLoadMoreButton();
+
+    return hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    Notiflix.Notify.failure(
+      'An error occurred while fetching images. Please try again.'
+    );
+    return [];
+  }
 }
 
 function renderImages(images) {
-    if (page === 1) {
-        gallery.innerHTML = '';
-    }
+  if (page === 1) {
+    gallery.innerHTML = '';
+  }
 
-    images.forEach((image) => {
-        const card = createImageCard(image);
-        gallery.appendChild(card);
-    });
+  images.forEach(image => {
+    const card = createImageCard(image);
+    gallery.appendChild(card);
+  });
 
-    showLoadMoreButton();
-    updateImageWidth();
+  showLoadMoreButton();
+  updateImageWidth();
 
-    lightbox.refresh();
+  lightbox.refresh();
 }
 
 function createImageCard(image) {
-    const card = document.createElement('div');
-    card.classList.add('photo-card');
+  const card = document.createElement('div');
+  card.classList.add('photo-card');
 
-    const img = document.createElement('img');
-    img.src = image.webformatURL;
-    img.alt = image.tags;
-    img.loading = 'lazy';
+  const img = document.createElement('img');
+  img.src = image.webformatURL;
+  img.alt = image.tags;
+  img.loading = 'lazy';
 
-    const info = document.createElement('div');
-    info.classList.add('info');
-    info.innerHTML = `
+  const info = document.createElement('div');
+  info.classList.add('info');
+  info.innerHTML = `
         <p class="info-item"><b>Likes:</b> <span>${image.likes}</span></p>
         <p class="info-item"><b>Views:</b> <span>${image.views}</span></p>
         <p class="info-item"><b>Comments:</b> <span>${image.comments}</span></p>
         <p class="info-item"><b>Downloads:</b> <span>${image.downloads}</span></p>
     `;
+    const link = document.createElement('a');
+    link.href = image.webformatURL; // Додайте цей рядок
+    link.appendChild(img);
 
-    card.appendChild(img);
-    card.appendChild(info);
+    card.appendChild(link);
+link.href = image.webformatURL; 
+  link.appendChild(img);
 
-    info.style.display = 'flex';
-    info.style.width = '25%';
-    info.style.gap = '5px';
-    info.style.fontSize = '0.6em';
-    info.style.textAlign = 'center';
+  card.appendChild(link);
+  card.appendChild(info);
 
-    return card;
+  info.style.display = 'flex';
+  info.style.width = '25%';
+  info.style.gap = '5px';
+  info.style.fontSize = '0.6em';
+  info.style.textAlign = 'center';
+
+  return card;
 }
 
 function showLoadMoreButton() {
-    loadMoreBtn.style.display = 'block';
+  loadMoreBtn.style.display = 'block';
 }
 
 function hideLoadMoreButton() {
-    loadMoreBtn.style.display = 'none';
+  loadMoreBtn.style.display = 'none';
 }
 
 function updateImageWidth() {
-    const photoCards = document.querySelectorAll('.photo-card');
+  const photoCards = document.querySelectorAll('.photo-card');
 
-    photoCards.forEach((card) => {
-        const img = card.querySelector('img');
-        const containerWidth = card.clientWidth;
+  photoCards.forEach(card => {
+    const img = card.querySelector('img');
+    const containerWidth = card.clientWidth;
 
-        img.style.width = containerWidth + 'px';
-    });
+    img.style.width = containerWidth + 'px';
+  });
 }
-
